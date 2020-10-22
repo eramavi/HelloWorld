0d6d0cd2ae351d7ba9377de8520d557302191d64
Raman Kumar Vishwakarma <raman.kumar.vishwakarma@ericsson.com>
2020-10-22 13:36:44 +0530

    some changes

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,8 +4,7 @@ public class HelloWorld {
   public static void main(final String[] args) {
     for(int i=0;i<=10;i++){
 		System.out.println("Hello, World! The current counter is " + i);
-	}
-    
+	}    
   }
 
 }
